e0ab194a0174c2b4bf40981d7414b3aefb607cba
Alex Gregg <agregg444@gmail.com>
2019-02-27 12:10:33 -0600

    Add project files.

---
+++ Module06_Assignment_OOPaM_aGregg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module06_Assignment_OOPaM_aGregg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
