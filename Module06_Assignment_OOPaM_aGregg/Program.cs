2538ad01be881a49ed1d8fc867a05d9b4fcfd2d7
Alex Gregg <agregg444@gmail.com>
2019-03-03 18:25:16 -0600

    Assignment_M06 Initial Commit

--- Module06_Assignment_OOPaM_aGregg/Program.cs
+++ Module06_Assignment_OOPaM_aGregg/Program.cs
@@ -1,12 +1,202 @@
 ﻿using System;
 
+//TaskBook
+//Task tracker for nothing specific
+/*  Homework Index:
+ * ARRAY - Line: 27 Array of Task Objects
+ * METHODS - This class Lines: 60, 88, 108, 147. In Task.cs Lines: 46, 53, 57, 81, 85
+ * CLASSES - Task.cs file Lines: 8 and 65
+ * INHERITANCE - Task.cs file Line: 65
+ * TRY-CATCH - Lines: 142-165
+ * RECURSION - Line: starts at 70 -- ShowTasksMethod
+ */
+
 namespace Module06_Assignment_OOPaM_aGregg
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool openBook = true;
+
+            //Heres an example of Arrays and polymorphism
+            //IMO: Very messy, declaring tons of empty task objects that might not be used.
+            //Also limits how many tasks can be used.
+            Task[] tasks = new Task[100];
+            tasks[0] = new Task();
+            tasks[1] = new Task("Zimbabwe", "Fly to zimbabwe");
+            tasks[2] = new TimedTask("Finish Game UI", "Properly implement buttons and menus systems", DateTime.Now.Add(new TimeSpan(32, 0, 0, 0)));
+            tasks[3] = new Task("Read Book", "Read that one book you havent picked up in over 5 years.");
+            
+
+            Task teesk = new TimedTask("blah", "polymorphism can get nasty in languages other than python, and even then....",DateTime.Now.Add(new TimeSpan(4,12,32,1)));
+
+            tasks[0] = teesk;   //Replacing the first empty task with a TimedTask.
+
+            int inpt_num = 0;
+            while (openBook)
+            {
+                Console.WriteLine("\n\n==========Task Book==========");
+                Console.WriteLine("\tView Tasks: 0\n\tAdd Task: 1\n\tAdd Timed Task: 2\n\tRemove task: 3\n\tQuit: 4");
+                //Fails neatly
+                int.TryParse(Console.ReadLine(),out inpt_num);
+                switch (inpt_num)
+                {
+                    case 0:
+                        int i = 0;
+                        ShowTasks(tasks, i);
+                        break;
+                    case 1:
+                        AddTask(ref tasks);
+                        break;
+                    case 2:
+                        AddTimedTask(ref tasks);
+                        break;
+                    case 3:
+                        RemoveTask(ref tasks);
+                        break;
+                    case 4:
+                        openBook = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choice invalid, please try again.");
+                        break;
+                }
+            }
+            
+        }
+
+        //RECURSION EXAMPLE
+        public static void ShowTasks(Task[] tasks, int i)
+        {
+            
+            if(i < tasks.Length)
+            {
+                
+            
+                if (tasks[i] != null)
+                {
+                    if (tasks[i].name != "")
+                    {
+                        Console.WriteLine("------------------------------------------");
+                        Console.WriteLine($"Task ID: {tasks[i].ID.ToString()}\nTaskName: {tasks[i].name}\nTaskDescription:\n\t{tasks[i].description}\nDate Created: {tasks[i].created.ToString()}");
+
+                        //A very backwards way of working around the polymorphism problem while avoiding multiple arrays.
+                        //Rather than using if t.getType() == TimedTask, im stuck adding a property to all task objects
+                        if (tasks[i].taskType == "timed")
+
+                        {
+                            Console.WriteLine($"Finish by: {tasks[i].goal.ToString()}");
+                            if (tasks[i].finished != true)
+                            {
+                                Console.WriteLine("Incomplete");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Complete");
+                            }
+                        }
+                    }
+                }
+                i++;
+                ShowTasks(tasks, i);
+            }
+            
+        }
+        public static void AddTask(ref Task[] tasks)
+        {
+            string t_name = "";
+            string t_desc = "";
+
+            Console.Write("Your Task: ");
+            t_name = Console.ReadLine();
+            Console.Write("Task description: ");
+            t_desc = Console.ReadLine();
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                if (tasks[i] == null)
+                {
+                    tasks[i] = new Task(t_name, t_desc);
+                    break;
+                }else if(tasks[i].name == "")
+                {
+                    tasks[i] = new Task(t_name, t_desc);
+                    break;
+                }
+            }
+
+        }
+        public static void AddTimedTask(ref Task[] tasks)
+        {
+
+            DateTime t_goal = DateTime.Now;
+            TimeSpan time;
+            int days = 0;
+            string t_name = "";
+            string t_desc = "";
+
+            // THE TRY CATCH EXAMPLE
+            
+            try
+            {
+                Console.Write("Your Task: ");
+                t_name = Console.ReadLine();
+                Console.Write("Task description: ");
+                t_desc = Console.ReadLine();
+                Console.Write("How many days until the task should be finished?: ");
+            
+                int.TryParse(Console.ReadLine(), out days);
+                time = new TimeSpan(days, 0, 0, 0);
+                t_goal = DateTime.Now.Add(time);
+            }
+            catch (TimeZoneNotFoundException tznfe)
+            {
+                throw tznfe;
+            }
+            catch (System.IO.IOException ioe)
+            {
+                Console.WriteLine($"{ioe.Message}\n Input was incorrect, please try again.");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                if (tasks[i] == null)
+                {
+                    tasks[i] = new TimedTask(t_name, t_desc, t_goal);
+                    break;
+                }else if (tasks[i].name == "")
+                {
+                    tasks[i] = new TimedTask(t_name, t_desc, t_goal);
+                    break;
+                }
+            }
+        }
+        //The tasks array will never get smaller but we can atleast substitute the tasks
+        public static void RemoveTask(ref Task[] tasks)
+        {
+            int id;
+            Console.Write("ID of task to delete: ");
+
+            int.TryParse(Console.ReadLine(),out id);
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                if(tasks[i].ID == id)
+                {
+                    tasks[i] = new Task(id);
+                    break;
+                }
+                else if(tasks[i].ID != id && i == tasks.Length - 1)
+                {
+                    Console.WriteLine("There are no tasks with that ID\nPlease check the task ID again.");
+                }
+            }
         }
     }
 }
---
+++ Module06_Assignment_OOPaM_aGregg/Task.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module06_Assignment_OOPaM_aGregg
+{
+    //MY EXAMPLE OF CLASSES
+    class Task
+    {
+        //Properties
+        public static int ID_HOLD { get; protected set; } = 0; // A static property to be called by each new Task instance
+        public int ID { get; protected set; } = 0;
+        public string name { get; set; } = "";
+        public string description { get; set; } = "";
+        public DateTime created { get; protected set; }
+        public DateTime goal { get; set; }  
+        public bool finished { get; set; } = false;
+        public string taskType { get; protected set; } = "standard";
+
+        //Constructors
+        //Default contructor
+        public Task()
+        {
+            //More than one task will have the same ID but no unique task will share the same id
+            ID = ID_HOLD;
+            
+        }
+        //Overloaded constructor to prevent excess IDs from being generated when old ones are "erased".
+        public Task(int old)
+        {
+            ID = old;
+
+        }
+        //Overloaded constructor
+        public Task(string t_name, string t_desc)
+        {
+            ID = Gen_ID();
+
+            name = t_name;
+            description = t_desc;
+            created = DateTime.Now;
+        }
+
+
+        //protected static memeber method for creating the next ID
+        protected static int Gen_ID()
+        {
+            //Generates the ID for the next task
+            return ID_HOLD += 1;
+        }
+
+        //Public member methods
+        public void Rename(string t_rename)
+        {
+            name = t_rename;
+        }
+        public void Redescribe(string t_redescription)
+        {
+            description = t_redescription;
+        }
+
+    }
+
+    //MY EXAMPLE OF INHERITANCE
+    //The inherited TimedTask class inherits all Task properties and methods (no constructors though)
+    class TimedTask : Task
+    {
+        
+        //A constructor for an inherited class
+        public TimedTask(string t_name, string t_desc, DateTime t_goal)
+        {
+            ID = Gen_ID();
+            name = t_name;
+            description = t_desc;
+            created = DateTime.Now;
+            goal = t_goal;
+            taskType = "timed";
+        }
+
+        //Heres an example of a public method accessing an otherwise protected property
+        //Might normally extend the date by setting a further date rather than adding time.
+        public void ExtendGoal(TimeSpan time)
+        {
+            goal.Add(time);
+        }
+        public void FinishGoal()
+        {
+            finished = true;
+        }
+    }
+}
